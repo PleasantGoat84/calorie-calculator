d5a5c74faeae2da594c9b00a3d94771e0f0888f1
PleasantGoat84 <PleasantGoat84@gmail.com>
2020-10-11 21:48:28 +0800

    table selection

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
+    <v-navigation-drawer app v-model="drawer" temporary>
       <!-- -->
     </v-navigation-drawer>
 
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon />
+    <v-app-bar app color="primary darken-2" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
       <v-toolbar-title>製作我的餐單</v-toolbar-title>
 
       <template v-slot:extension>
-        <v-tabs>
+        <v-tabs v-model="tab">
           <v-tab v-for="(kind, i) in foodKinds" :key="i">
             {{ kind }}
           </v-tab>
@@ -21,9 +21,42 @@
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
+      <v-container fluid class="px-0">
+        <!-- <v-tabs-items v-model="tab">
+          <v-tab-item v-for="(kind, i) in foodKinds" :key="i">
+            <v-card color="info" class="white--text mx-auto">
+              <v-card-title>
+                <h2>{{ kind }}</h2>
+              </v-card-title>
+
+              <v-data-table
+                v-model="selected[i]"
+                class="rounded-0"
+                show-select
+                :headers="tableHeaders"
+                :items="foodData[kind]"
+                item-key="name"
+                :items-per-page="15"
+              />
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items> -->
+
+        <v-card color="info" class="white--text mx-auto">
+          <v-card-title>
+            <h2>{{ foodKinds[tab] }}</h2>
+          </v-card-title>
+
+          <v-data-table
+            v-model="selected"
+            class="rounded-0"
+            show-select
+            :headers="tableHeaders"
+            :items="foodData[foodKinds[tab]]"
+            item-key="name"
+            :items-per-page="15"
+          />
+        </v-card>
       </v-container>
     </v-main>
 
@@ -39,6 +72,16 @@ import foodData from "./data/food";
 
 @Component
 export default class CalorieCalc extends Vue {
+  private tab = 0;
+  private drawer = false;
+  private selected = [];
+
+  private readonly tableHeaders = [
+    { text: "食品名稱", value: "name" },
+    { text: "份量", value: "quantity" },
+    { text: "卡路里", value: "calorie" }
+  ];
+
   readonly foodData = foodData;
   get foodKinds() {
     return Object.keys(this.foodData);
@@ -57,5 +100,12 @@ export default class CalorieCalc extends Vue {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.v-card {
+  width: 95%;
+  @media (min-width: 1000px) {
+    width: 85%;
+  }
 }
 </style>
